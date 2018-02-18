761cf30476698fd2f8a3716f735dbdd3136652d6
Ben Siver <ben.siver@sproutsocial.com>
2018-02-18 12:55:47 -0600

    Update schema to 1.1.7

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.6"]]
+                 [prismatic/schema "1.1.7"]]
   :main ^:skip-aot okcampkid.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
