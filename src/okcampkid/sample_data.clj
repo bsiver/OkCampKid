0d429ce9a0c36f78e1b75778d14d2baef310310e
Ben Siver <ben.siver@sproutsocial.com>
2018-06-11 12:07:41 -0500

    Add more sample data

--- src/okcampkid/sample_data.clj
+++ src/okcampkid/sample_data.clj
@@ -30,5 +30,31 @@
    :instrument :drums
    :preferences []})
 
+(def mary-camper
+  {:name "Mary"
+   :age 10
+   :instrument :bass
+   :preferences []})
+
+(def patty-camper
+  {:name "Patty"
+   :age 10
+   :instrument :keys
+   :preferences []})
+
+(def marley-camper
+  {:name "Marley"
+   :age 14
+   :instrument :guitar
+   :preferences []})
+
+(def sam-camper
+  {:name "Sam"
+   :age 10
+   :instrument :drums
+   :preferences []})
+
 (def family-band {:campers [ben-camper jess-camper paul-camper cupcake-camper]})
-(def all-campers [ben-camper jess-camper cupcake-camper paul-camper jane-drummer])
+(def other-band {:campers [marley-camper mary-camper patty-camper sam-camper]})
+(def all-campers [ben-camper jess-camper cupcake-camper paul-camper jane-drummer marley-camper mary-camper
+                  patty-camper marley-camper sam-camper])
